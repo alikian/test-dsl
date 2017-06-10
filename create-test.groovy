368ecf6c8d32bf240d0affb8c6d04f38df33e0e3
Ali Kian Zadeh <alikian@gmail.com>
2017-06-10 10:54:13 -0700

    enhanced

--- create-test.groovy
+++ create-test.groovy
@@ -1,14 +1,31 @@
-def services = ["Apple", "Banana", "Orange"]
-def jobBase = 'sa-ca-test'
+def services = ["cst", "customer", "environment","orchestrator","reactor","resolver","rule","solution"]
+def jobBase = 'sa-ca-aws-deploy-app-{}-dev'
 services.each {
     def service = it
-    def jobName = "${jobBase}-${service}"
+    def jobName = "sa-ca-aws-deploy-app-${service}-dev"
     job(jobName) {
+        parameters {
+          password{
+            name('AWS_ACCESS_KEY_ID')
+            defaultValue('')
+            description('Enter your key ID to access AWS')
+          }
+          password{
+            name('AWS_SECRET_ACCESS_KEY')
+            defaultValue('')
+            description('Enter your secret key to access AWS')
+          }
+          string{
+            name('APP_DEPLOYER_VERSION')
+            defaultValue('0.0.1-SNAPSHOT')
+            description('app deployer template zip version')
+          }
+        }
         scm {
             git("git://github.com/${service}.git", 'develop')
         }
         steps {
-            maven("test -Dproject.name={service}")
+          maven("dependency:unpack -Dartifact=com.teradata.tmc.sa.app:app-deployer:\${APP_DEPLOYER_VERSION}:zip -DoutputDirectory=.")
         }
     }
 }
